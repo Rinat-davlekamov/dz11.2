7d12a987d208fb57b03314040454b6200211d7e0
rinat_davlekamov <rinat_davlekamov@mail.ru>
2024-03-31 13:37:22 +0400

    add first_liter_up

--- str_func.py
+++ str_func.py
@@ -1,4 +1,14 @@
 def upper(word):
+    """
+    Верхний регистр
+    :param word:
+    :return:
+    """
     return word.upper()
 
 
+def first_liter_up(offer):
+    """"
+    делает заглавными первые буквы каждого слова в строке
+    """
+    return offer.title()
